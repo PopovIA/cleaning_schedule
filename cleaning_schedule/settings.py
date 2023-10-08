e8fc1e74572b5ccf125bde16b9cc261884135c03
PopovIA <popovi-87@mail.ru>
2023-10-08 11:01:49 +0300

    ...

--- cleaning_schedule/settings.py
+++ cleaning_schedule/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -30,6 +29,16 @@ ALLOWED_HOSTS = ['45.136.245.37', '127.0.0.1']
 # Celery settings
 CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_TIMEZONE = "Europe/Moscow"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_IMPORTS = ('reporter.tasks', 'mdlp.tasks',)
+# CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+# CELERY_RESULT_BACKEND = 'django-db'
+CELERYD_OPTS = "--time-limit=9600 -E --loglevel=DEBUG"
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+DJANGO_CELERY_BEAT_TZ_AWARE = False
+
 
 # Application definition
 
@@ -41,7 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cleaning_schedule',
-    'main',
+    'main.apps.MainConfig',
+    'django_celery_beat',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -75,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cleaning_schedule.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -85,11 +95,10 @@ DATABASES = {
         'NAME': 'cleaning',
         'USER': 'djangoadm',
         'PASSWORD': '19871115PiLL!',
-        'HOST': '45.136.245.37',   # Or an IP Address that your DB is hosted on
+        'HOST': '45.136.245.37',  # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -109,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -120,7 +128,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
